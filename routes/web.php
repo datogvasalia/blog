e7a5f05e54688e81175d330c4d15f625e9bc0208
vaskita101 <vaska.r02@gmail.com>
2018-05-10 15:30:45 +0400

    removred

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('test', function() {
-    return 'test from vaska';
-});
 
 Route::get('dato', function() {
     return 'test from dato';
